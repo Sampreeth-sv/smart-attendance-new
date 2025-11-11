52d8784a88d965a28e56360bf4a6cc4092d08575
Sampreeth-sv <sampreeth.sv@gmail.com>
2025-11-11 20:14:05 +0530

    Delete backend directory

--- backend/train_face_model.py
+++
@@ -1,6 +0,0 @@
-# CLI script to train face model (run manually)
-from services.facial_service import train_model
-
-if __name__ == "__main__":
-    ok = train_model()
-    print("Training finished:", ok)
